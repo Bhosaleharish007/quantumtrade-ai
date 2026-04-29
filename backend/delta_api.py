e251de0d40b3e9c0ae82b7af5d485c8ddb88905f
Bhosaleharish007 <3hosaleh@gmail.com>
2026-04-29 23:16:10 +0530

    Update delta_api.py

--- backend/delta_api.py
+++ backend/delta_api.py
@@ -1,1 +1,12 @@
+import random
 
+def get_balance():
+    return {
+      'balance':1000
+    }
+
+def get_positions():
+    return []
+
+def get_mark_price(symbol='BTCUSDT'):
+    return 95000 + random.randint(-500,500)
