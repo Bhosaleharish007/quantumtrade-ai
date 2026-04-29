4396a3687e0d53879b54c0e76889e5c0c1d9d77c
Bhosaleharish007 <3hosaleh@gmail.com>
2026-04-29 23:12:52 +0530

    Update BotControls.jsx

--- frontend/src/BotControls.jsx
+++ frontend/src/BotControls.jsx
@@ -1,1 +1,32 @@
+import React from "react";
 
+export default function BotControls(){
+
+const startBot=()=>{
+fetch('/api/start',{
+method:'POST'
+})
+}
+
+const stopBot=()=>{
+fetch('/api/stop',{
+method:'POST'
+})
+}
+
+return(
+<div>
+<h2>Bot Controls</h2>
+
+<button onClick={startBot}>
+Start Bot
+</button>
+
+<button onClick={stopBot}>
+Stop Bot
+</button>
+
+</div>
+)
+
+}
