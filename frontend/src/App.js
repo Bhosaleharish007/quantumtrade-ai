58130581dc7699bda6cf0db16ae7f6299189e907
Bhosaleharish007 <3hosaleh@gmail.com>
2026-04-29 23:13:19 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,16 @@
+import ChartPanel from './components/ChartPanel';
+import BotControls from './components/BotControls';
 
+function App(){
+return(
+<div>
+<h1>QuantumTrade AI Dashboard</h1>
+
+<ChartPanel/>
+<BotControls/>
+
+</div>
+)
+}
+
+export default App;
