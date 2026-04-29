8e3bd3dcc02d64d72e1d3648451b8fee2500b67f
Bhosaleharish007 <3hosaleh@gmail.com>
2026-04-29 23:41:55 +0530

    Update PositionsTable.jsx

--- frontend/src/PositionsTable.jsx
+++ frontend/src/PositionsTable.jsx
@@ -1,1 +1,11 @@
-
+export default function PositionsTable(){
+return(
+<div>
+<h2>Open Positions</h2>
+<table>
+<tbody>
+<tr><td>BTCUSDT</td><td>No Open Positions</td></tr>
+</tbody>
+</table>
+</div>
+)}
